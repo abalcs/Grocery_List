f0030c44b5652d91e20e3190962557bfe5f4aa4b
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-12-30 23:22:38 -0500

    debug

--- server/server.js
+++ server/server.js
@@ -11,10 +11,13 @@ const app = express(); // instance = Object
 
 // Setup our server
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-  })
+
+  app.use('/', express.static(path.join(__dirname, '/client/build')));
+
+  // app.use(express.static('client/build'));
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  // })
 }
 // app.use(express.static(path.join(__dirname, 'client/build')));
 
