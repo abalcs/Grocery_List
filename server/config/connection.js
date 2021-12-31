a85a29e0bad1ea80362e70d21f80703adeb7312c
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-12-30 20:42:33 -0500

    updating db code to connect

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/groceries');
+mongoose.connect('mongodb://localhost/groceries');
 
 module.exports = mongoose.connection;
