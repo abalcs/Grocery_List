f9f7503948f9d220d0a16fef6393af368d5bf314
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-12-31 08:28:36 -0500

    debug

--- server/server.js
+++ server/server.js
@@ -13,14 +13,14 @@ const app = express(); // instance = Object
 
 // Setup our server
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use('/', express.static(path.join(__dirname, 'client/build')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 routes(app);
 
