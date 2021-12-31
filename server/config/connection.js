46313815bf5cbdc9b713be8c19fc326525bb7ef0
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-12-30 23:58:41 -0500

    debug

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/groceries', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/groceries', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
