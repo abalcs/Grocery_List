a43dedb00032f41af6de85d18f291e5c514d5604
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2022-01-02 22:34:18 -0500

    debug

--- server/server.js
+++ server/server.js
@@ -23,9 +23,15 @@ app.use(express.urlencoded({ extended: true }));
 
 routes(app);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-})
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 db.once('open', () => {
   // Start the Server
